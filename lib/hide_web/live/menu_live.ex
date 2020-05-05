33f906beed0f135ac10aa288ff039143e2de6d1f
Jason Axelson <jason.axelson@gmail.com>
2020-05-04 19:44:41 -1000

    LiveComponent reproduction with id

--- lib/hide_web/live/menu_live.ex
+++ lib/hide_web/live/menu_live.ex
@@ -6,30 +6,8 @@ defmodule HideWeb.MenuLive do
   end
 
   def render(assigns) do
-    options =
-      ["opt1", "option 2"]
-      |> Enum.with_index()
-
     ~L"""
-    <g transform="translate(50 100)" class="menu" id="myid">
-      <%= for {option, index} <- options do %>
-        <%= render_option(option, index, assigns) %>
-      <% end %>
-    </g>
+    <image id="image" href="<%= @href %>"></image>
     """
-  end
-
-  def render_option(option, index, assigns) do
-    ~L"""
-    <g transform="translate(0 <%= index * 50 %>)" phx-click="menu-click" phx-target="<%= @myself %>">
-    <text><%= option %></text>
-    </g>
-    """
-  end
-
-  def handle_event("menu-click", _event, socket) do
-    IO.inspect(socket.assigns, label: "socket.assigns")
-    IO.puts("Handle menu click!")
-    {:noreply, assign(socket, set_time: true)}
   end
 end
